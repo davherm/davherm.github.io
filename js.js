02d10ac7fc4b89ba21f00ba85d253a1f82a4c186
skaduddel <david.hermansson1998@gmail.com>
2016-11-14 14:53:48 +0100

    second push

--- js.js
+++ js.js
@@ -1,47 +1,223 @@
-var KEY = { a:65, d:68, s:83, w:87 };
-var move = 80;
-var snake = document.getElementById("huve");
-var collision = false;
-var delay = 1000;
 
-/*do {
-    setTimeout(function() {
-        snake.style.transform += "translateX("+ (move) + "px)";
-    }, delay);
-} while (collision===false);*/
+var svgNS = "http://www.w3.org/2000/svg";
+var xlinkNS = "http://www.w3.org/1999/xlink";
 
-document.documentElement.addEventListener('keydown',function(press){
+var CurrentDir='S';
+var ThisDir='S';
+var LastUpdate=(new Date()).getTime();
+var GameOver=true;
+var Paused=false;
+var MyInterval=null;
+var HeadX=10;
+var HeadY=40;
+var FoodX=10;
+var FoodY=10;
+var MoveCount=0;
+var ThisLevel=1;
+var Score=0;
 
+var Snake;
+var Player;
+var Head;
+var Food;
+var ScoreBoard;
+var Splash;
+var Text1;
+var Text2;
+var PausedText;
+var FeedBack;
+var FeedBack2;
 
+function ShowSplash(T1,T2)
+{
+    Text1.nodeValue=T1;
+    Text2.nodeValue=T2;
+    Splash.setAttribute('visibility','visible');
+}
 
-    switch(press.keyCode) {
-        case 65:
-            snake.style.transform += "translateX("+ (-move) + "px)";
-            break;
+function Init()
+{
+    Player=document.getElementById('Player');
+    Head=document.getElementById('Head');
+    Food=document.getElementById('Food');
+    Splash=document.getElementById("Splash");
+    Text1=document.getElementById("Text1").firstChild;
+    Text2=document.getElementById("Text2").firstChild;
+    PausedText=document.getElementById("Paused");
+    ScoreBoard=document.getElementById("ScoreBoard").firstChild;
+    ShowSplash('SNAKE','press enter to play');
+}
 
-        case 68:
+function NewGame()
+{
+    Splash.setAttribute('visibility','hidden');
+    Score=0;
+    ScoreBoard.nodeValue='Score: 0';
+    ThisLevel=1;
+    ResetSnake();
+    MoveFood();
+    GameOver=false;
+    MyInterval=setInterval("update()", 10);
+}
 
-                    snake.style.transform += "translateX("+ (move) + "px)";
+function ResetSnake()
+{
+    Snake=new Array();
+    Snake[0]='10,40';
+    Snake[1]='10,30';
+    Snake[2]='10,20';
+    Snake[3]='10,10';
+    HeadX=10;
+    HeadY=40;
+    ThisDir='S';
+}
 
-            break;
+function MoveFood() //placera maten slumpat inom svg-ramen
+{
+    var PathStr=Snake.join(' ');
+    PathStr+=' ';
+    while(PathStr.indexOf(FoodX+','+FoodY)>-1)
+    {
+        FoodX=Math.round(Math.random()*40)*10;
+        FoodY=Math.round(Math.random()*30)*10;
+    }
+    Food.setAttribute("cx",FoodX);
+    Food.setAttribute("cy",FoodY);
+}
+
+function update()//så länge ormen lever och spelet inte är pausat så fortsätter uppdateringen
+{
+    if(!GameOver)
+    {
+        if(!Paused)
+        {
+            if((new Date()).getTime()-LastUpdate>50) //hur fort ormen åker, 50ms uppdatering
+            {
+                Move();
+                LastUpdate=(new Date()).getTime();
+            }
+        }
+    }
+    else
+    {
+        window.clearInterval(MyInterval);
+        GameOver=true;
+        ShowSplash('Game Over','press enter to replay');
+    }
+}
+
+function Move()//här ändrar man de flesta av snakes attributer ex. längd, riktning och position
+{
+    CurrentDir=ThisDir;
+
+    var MyAngle=0; //börja med att peka neråt, SOUTH(S)
+    if(ThisDir=='N')
+    {
+        HeadY=HeadY-10;
+        MyAngle=180;
+    }
+    else if(ThisDir=='E')
+    {
+        HeadX=HeadX+10;
+        MyAngle=270;
+    }
+    else if(ThisDir=='S')
+    {
+        HeadY=HeadY+10;
+        MyAngle=0;
+    }
+    else if(ThisDir=='W')
+    {
+        HeadX=HeadX-10;
+        MyAngle=90;
+    }
+    var HeadStr=HeadX;
+    HeadStr+=',';
+    HeadStr+=HeadY;
+    var PathStr='M';
+    PathStr+=HeadStr;
+    PathStr+=' ';
+
+    if(HeadX==FoodX && HeadY==FoodY) { //när huvudet kolliderar med maten så kommer snake bli längre
+        MoveCount=MoveCount+10^4;
+    }
+    else if(MoveCount<10^4) //10000 är en symbolisk siffra
+    {
+        Snake.pop();
+        MoveCount++;
+    }
+    else
+        MoveCount=0;
+
+    PathStr+=Snake.join(' L');
+    PathStr+=' ';
+
+    Snake.unshift(HeadStr);
+
+    if(HeadX<0 || HeadX>400 || HeadY<0 || HeadY>300 || PathStr.indexOf('L'+HeadStr+' ')>-1) //game over om snake har åkt in i en vägg eller sig själv
+    {
+        GameOver=true;
+        return;
     }
 
+    Player.setAttribute('d',PathStr);
+    Head.setAttribute('transform','translate('+HeadX+','+HeadY+') rotate('+MyAngle+',0,0)');
 
-/*var x = pacman.getAttribute('x')*1,
-    y = pacman.getAttribute('y')*1,
-    direction = "right";
+    if(HeadX==FoodX && HeadY==FoodY) //öka poäng med 1 om huvudet har åkt in i en matbit
+    {
+        MoveFood();
+        Score++;
+        ScoreBoard.nodeValue='Score: '+Score;
+    }
+}
 
-    if (press.keyCode === 65 && direction === "left") {
-        pacman.style.transform += "translateX("+ (move) + "px)";
-    } else if (press.keyCode === 65 && direction === "right") {
-        pacman.style.transform += "translateX("+ (move) + "px)";
-        pacman.style.transform += "rotateY("+ (180) +"deg)";
-        direction = "left";
-    } else if (press.keyCode === 68 && direction === "right") {
-        pacman.style.transform += "translateX("+ (move) + "px)";
-    } else if (press.keyCode === 68 && direction === "left") {
-        pacman.style.transform += "translateX("+ (move) + "px)";
-        pacman.style.transform += "rotateY("+ (180) +"deg)";
-        direction = "right";
-    }*/
-},false);
+function KeyPress(evt)//Hindrar snake från att kunna göra 180graders sväng
+{
+    if(evt.keyCode==37 && CurrentDir!='E') // 37=West
+    {
+        evt.preventDefault();
+        ThisDir='W';
+    }
+    else if(evt.keyCode==38 && CurrentDir!='S') // 38=North
+    {
+        evt.preventDefault();
+        ThisDir='N';
+    }
+    else if(evt.keyCode==39 && CurrentDir!='W') // 39=East
+    {
+        evt.preventDefault();
+        ThisDir='E';
+    }
+    else if(evt.keyCode==40 && CurrentDir!='N') // 40=South
+    {
+        evt.preventDefault();
+        ThisDir='S';
+    }
+    else if(evt.keyCode==80) // Pause
+    {
+        evt.preventDefault();
+        if(!Paused)
+        {
+            Paused=true;
+            ShowSplash('Paused','press enter to resume');
+        }
+        else
+        {
+            Paused=false;
+            Splash.setAttribute('visibility','hidden');
+        }
+    }
+    else if(evt.keyCode==13 && (GameOver || Paused))  // 13=enter - starta nytt spel eller forsätt där spelaren pausat
+    {
+        evt.preventDefault();
+        if(Paused)
+        {
+            Paused=false;
+            Splash.setAttribute('visibility','hidden');
+        }
+        else
+            NewGame();
+    }
+}
+
+document.documentElement.addEventListener("keydown",KeyPress,false)();
